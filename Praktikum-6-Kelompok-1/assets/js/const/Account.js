c6bd68282fe13f11da1859f78a9ae8915f42eaab
Michahide <73429490+Michahide@users.noreply.github.com>
2022-06-04 13:06:23 +0700

    Praktikum-6

---
+++ Praktikum-6-Kelompok-1/assets/js/const/Account.js
@@ -0,0 +1,8 @@
+const MyAccount = {
+    id:3320190306,
+    name:"Febri Damatraseta Fairuz, S.T, M.Kom",
+    email:"febrid@ibik.ac.id",
+    password:"Test123!!",
+    address:"Tangerang",
+    role:{id:1, name:"Lecturer"}
+}
